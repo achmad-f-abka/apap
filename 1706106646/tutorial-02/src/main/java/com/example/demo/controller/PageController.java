2fb2f4571c52e231a76f13c176f372350478f5f2
abasjr <anggitbaskoro7@gmail.com>
2019-03-09 18:18:09 +0700

    Calcu-Convert

--- 1706106646/tutorial-02/src/main/java/com/example/demo/controller/PageController.java
+++ 1706106646/tutorial-02/src/main/java/com/example/demo/controller/PageController.java
@@ -32,4 +32,47 @@ public class PageController {
         return "hello2";
     }
 
+    /*
+     * Latihan Calcu-Convert 
+     * Penjumlahan dengan menerima parameter 2 buah angka.
+     * X untuk parameter angka yang pertama
+     * Y untuk parameter angka yang kedua
+     * W untuk wumbo.. aku wumboo.. kau wumboo.. wumboo
+     */
+    @RequestMapping("/calculator")
+    public String perkalian(@RequestParam(value = "x", required = false, defaultValue = "0") int x,
+                            @RequestParam(value = "y", required = false, defaultValue = "0") int y, Model model) {
+        model.addAttribute("x", x);
+        model.addAttribute("y", y);
+        model.addAttribute("result", x + y);
+
+        String[] thousands = { "", " Seribu", " Dua Ribu", " Tiga Ribu", " Empat Ribu", " Lima Ribu", " Enam Ribu",
+                          " Tujuh Ribu", " Delapan Ribu", " Sembilan Ribu" };
+        String[] hundreds= { "", " Seratus", " Dua Ratus", " Tiga Ratus", " Empat Ratus", " Lima Ratus", " Enam Ratus",
+                           " Tujuh Ratus", " Delapan Ratus", " Sembilan Ratus" };
+        String[] dozens = { "", " Belas", " Dua Puluh", " Tiga Puluh", " Empat Puluh", " Lima Puluh", " Enam Puluh",
+                           " Tujuh Puluh", " Delapan Puluh", " Sembilan Puluh" };
+        String[] number = { "", " Satu", " Dua", " Tiga", " Empat", " Lima", " Enam", " Tujuh", " Delapan", " Sembilan", " Sepuluh" };
+
+        int result = x + y;
+        if (result < 9999 && result > 0) {
+            int cmath, tzinf, netrc, inflecto;
+            cmath= result / 1000;
+            tzinf= (result % 1000) / 100;
+            netrc= (result % 100) / 10;
+            inflecto= result % 10;
+            if (netrc== 1) {
+                if (inflecto== 1) {
+                    model.addAttribute("words", thousands[cmath] + hundreds[tzinf] + " Se" + dozens[netrc]);
+                } else {
+                    model.addAttribute("words", thousands[cmath] + hundreds[tzinf] + number[inflecto] + dozens[netrc]);
+                } 
+            } else {
+                model.addAttribute("words", thousands[cmath] + hundreds[tzinf] + dozens[netrc] + number[inflecto]);
+            }
+        } else {
+            model.addAttribute("words", "Converter hanya bekerja pada angka dibawah 10 Ribu");
+        }
+        return "calculator";
+    }
 }
