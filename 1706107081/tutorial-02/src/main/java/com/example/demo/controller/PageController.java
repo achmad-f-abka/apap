cd3480941d78fc0426bad86bcbfd4ac9a9054174
wiwin.nuraini <wiwin.nuraini@ui.ac.id>
2019-03-10 21:05:22 +0700

    Wiwin Nuraini - 1706107081

---
+++ 1706107081/tutorial-02/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,77 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	private String result;
+
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+	
+	
+	
+	@RequestMapping("/hello2")
+	public String hello2(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello2";
+	}
+	
+	@RequestMapping(value= {"/hello2", "/hello2/{name}"})
+	public String helloPath(@PathVariable Optional<String> name, Model model) {
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());
+		} else {
+			model.addAttribute("name", "Phoenix");
+		}
+		return "hello2";
+	}
+	
+	
+	@RequestMapping("/calculator/{firstNum}/{secondNum}")
+	public String calculator(@PathVariable int firstNum, @PathVariable int secondNum, Model model) {
+			model.addAttribute("firstNum", firstNum);
+			model.addAttribute("secondNum", secondNum);
+			model.addAttribute("totalSum", firstNum+secondNum);
+			int totalSum = firstNum + secondNum;
+			model.addAttribute("dibaca", convert(totalSum));
+		
+		return "calculator";
+	}
+	
+	
+	public String convert(@PathVariable Integer angka) {
+		String[] satuan = {"","satu","dua","tiga","empat","lima","enam","tujuh","delapan","sembilan"};
+		result = " ";
+		if (angka == 10) {
+			result = " sepuluh";
+		} else if (angka == 11) {
+			result = " sebelas";
+		} else if (angka < 10){
+            result = result + satuan[angka];
+        }else if (angka>11 && angka < 20){
+            result = result + convert(angka-10) + " belas";
+        }else if (angka < 100){
+            result = result + convert(angka / 10) + " puluh " + convert(angka % 10);
+        }else if (angka < 200){
+            result = result + "seratus " + convert(angka - 100);
+        }else if (angka < 1000){
+            result = result + convert(angka / 100) + " ratus "  + convert(angka % 100);
+        }else if (angka < 2000){
+            result = result + "seribu " + convert(angka - 1000);
+        }else{
+            result = result + convert(angka / 1000) + " ribu " + convert(angka % 1000);
+        }
+		return result;
+	}
+	
+	
+}
