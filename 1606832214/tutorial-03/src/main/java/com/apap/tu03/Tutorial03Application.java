b4516e26d3b3ca31bcfdc250e0314ea63a6cfb4d
Intan Wulandari <intan.wulandari61@ui.ac.id>
2019-03-17 20:45:00 +0700

    Intan Wulandari tutorial-03

--- 1606832214/tutorial-03/src/main/java/com/apap/tu03/Tutorial03Application.java
+++ 1606832214/tutorial-03/src/main/java/com/apap/tu03/Tutorial03Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.apap.tu03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tutorial02Application {
+public class Tutorial03Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Tutorial02Application.class, args);
+		SpringApplication.run(Tutorial03Application.class, args);
 	}
 
 }
--- 1606832214/tutorial-03/src/test/java/com/example/demo/Tutorial03ApplicationTests.java
+++ 1606832214/tutorial-03/src/test/java/com/example/demo/Tutorial03ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Tutorial02ApplicationTests {
+public class Tutorial03ApplicationTests {
 
 	@Test
 	public void contextLoads() {
