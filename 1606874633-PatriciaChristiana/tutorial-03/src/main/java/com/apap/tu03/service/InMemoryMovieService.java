db45e29bbf3c5470d06131d34a611923b52d4bd2
patriciachrst <36235642+patriciachrst@users.noreply.github.com>
2019-03-17 20:43:45 +0700

    Add files via upload

---
+++ 1606874633-PatriciaChristiana/tutorial-03/src/main/java/com/apap/tu03/service/InMemoryMovieService.java
@@ -0,0 +1,41 @@
+package com.apap.tu03.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.apap.tu03.model.MovieModel;
+
+@Service
+public class InMemoryMovieService implements MovieService {
+	private List<MovieModel> archiveMovie;
+	
+	public InMemoryMovieService() {
+		archiveMovie = new ArrayList<>();
+		
+	}
+	
+	@Override
+	public void addMovie(MovieModel movie) {
+		archiveMovie.add(movie);
+		
+	}
+
+	@Override
+	public List<MovieModel> getMovieList() {
+		return archiveMovie;
+	}
+
+	@Override
+	public MovieModel getMovieDetail(String id) {
+		for (MovieModel movie : archiveMovie) {
+			if (movie.getId().equalsIgnoreCase(id)) {
+				return movie;
+			}
+		}
+		
+		return null;
+	}
+
+}
