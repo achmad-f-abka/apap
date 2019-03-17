503076d9b2aba321bd4cfb19fea59d00e07d7918
sevirakarinap@gmail.com <sevirakarinap@gmail.com>
2019-03-17 22:50:49 +0700

    Tutorial-0r Sevira

---
+++ 1706107005/tutorial-03/src/main/java/com/apap/tu03/service/MovieService.java
@@ -0,0 +1,12 @@
+package com.apap.tu03.service;
+
+import java.util.List;
+import com.apap.tu03.model.MovieModel;
+
+public interface MovieService {
+	void addMovie (MovieModel movie);
+	void updateMovie(String id, Integer newDuration);
+	void deleteMovie(MovieModel movie);
+	List<MovieModel> getMovieList();
+	MovieModel getMovieDetail(String id);
+}
