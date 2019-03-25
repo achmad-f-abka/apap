8e0dfadca89c6fe6772c042ea2ed4b0a0401d39d
patriciachrst <patricianoni.pc@gmail.com>
2019-03-25 21:23:36 +0700

    tutorial 04

---
+++ 1606874633/tutorial-04/src/main/java/com/apap/tu04/repository/FlightDb.java
@@ -0,0 +1,22 @@
+	package com.apap.tu04.repository;
+	
+	import com.apap.tu04.model.FlightModel;
+import com.apap.tu04.model.PilotModel;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.stereotype.Repository;
+	
+	/**
+	 * 
+	 * PilotDb
+	 *
+	 */
+	@Repository
+	public interface FlightDb extends JpaRepository <FlightModel , Long>{
+		List <FlightModel> findByPilot (PilotModel pilot);
+		
+	}
+	
+	
