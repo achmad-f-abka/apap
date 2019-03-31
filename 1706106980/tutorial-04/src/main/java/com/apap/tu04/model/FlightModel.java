0c93dbe230495767e30a503c65348dedde5feaec
saryyuniastuti <saryyuniast@gmail.com>
2019-03-31 19:03:50 +0700

    Sary Yuni A Tutorial03

---
+++ 1706106980/tutorial-04/src/main/java/com/apap/tu04/model/FlightModel.java
@@ -0,0 +1,105 @@
+package com.apap.tu04.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="flight")
+public class FlightModel implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(max = 50)
+	@Column(name = "flight_number", nullable = false)
+	private String flightNumber;
+
+	@NotNull
+	@Size(max = 50)
+	@Column(name = "origin", nullable = false)
+	private String origin;
+
+	@NotNull
+	@Column(name = "destination", nullable = false)
+	private String destination;
+	
+	@NotNull
+	@Column(name = "time")
+	private String time;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="pilot_licenseNumber", referencedColumnName="license_number", nullable=false)
+	@OnDelete(action=OnDeleteAction.NO_ACTION)
+	@JsonIgnore
+	private PilotModel pilot;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public PilotModel getPilot() {
+		return pilot;
+	}
+
+	public void setPilot(PilotModel pilot) {
+		this.pilot = pilot;
+	}
+	
+	
+	
+}
