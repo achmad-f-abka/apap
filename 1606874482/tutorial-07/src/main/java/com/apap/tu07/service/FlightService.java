5555f307c5431db3d430c2b522087045f7fcd250
yuvitri.annisa <yuvitri14@gmail.com>
2019-05-05 23:44:00 +0700

    tutorial 07 1606874482

---
+++ 1606874482/tutorial-07/src/main/java/com/apap/tu07/service/FlightService.java
@@ -0,0 +1,24 @@
+package com.apap.tu07.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.apap.tu07.model.FlightModel;
+
+/**
+ * FlightService
+ */
+public interface FlightService {
+    FlightModel addFlight(FlightModel flight);
+    
+    void deleteByFlightNumber(String flightNumber);
+
+    Optional<FlightModel> getFlightDetailByFlightNumber(String flightNumber);
+    void updateFlight (Long flightId, FlightModel flight);
+    
+    void deleteFlight (FlightModel flight);
+    
+    List<FlightModel> getAllFlight();
+    Optional<FlightModel> getFlightById (long flightId);
+
+}
