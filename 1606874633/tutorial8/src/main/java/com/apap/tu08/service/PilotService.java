5d6ee04af53eade93c245c32a8d164a1f0e644c1
patriciachrst <patricianoni.pc@gmail.com>
2019-05-12 00:31:32 +0700

    tutorial-08 finished

---
+++ 1606874633/tutorial8/src/main/java/com/apap/tu08/service/PilotService.java
@@ -0,0 +1,21 @@
+package com.apap.tu08.service;
+
+import java.util.Optional;
+
+import com.apap.tu08.model.PilotModel;
+
+/**
+ * PilotService
+ */
+public interface PilotService {
+    Optional<PilotModel> getPilotDetailByLicenseNumber(String licenseNumber);
+
+    PilotModel addPilot(PilotModel pilot);
+
+    void deletePilotByLicenseNumber(String licenseNumber);
+
+    void deletePilot(PilotModel pilot);
+    
+    
+    Optional<PilotModel> getPilotDetailById(long id);
+}
