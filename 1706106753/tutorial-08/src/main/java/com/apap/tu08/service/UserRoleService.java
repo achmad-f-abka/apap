29ea3a84e769cf82628870003811fbdeebc32b1f
hannatriasilitonga <hanna.tria@ui.ac.id>
2019-05-13 01:08:09 +0700

    tutorial 08

---
+++ 1706106753/tutorial-08/src/main/java/com/apap/tu08/service/UserRoleService.java
@@ -0,0 +1,13 @@
+package com.apap.tu08.service;
+
+import com.apap.tu08.model.UserRoleModel;
+
+public interface UserRoleService {
+	UserRoleModel addUser(UserRoleModel user);
+
+	public String encrypt(String password);
+
+	/*UserRoleModel getUser(String name);
+
+	boolean isMatch(String rawPassword, String encodedPassword);*/
+}
