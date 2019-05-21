1af9ec6ec79cfc5249ca1c6d16716dc8b8d1808e
farisqadr <faris.code@gmail.com>
2019-05-21 17:34:30 +0700

    update

---
+++ 1706106734/tutorial-05/src/main/java/com/apap/tu04/service/FlightServiceImpl.java
@@ -0,0 +1,70 @@
+package com.apap.tu04.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.tu04.model.FlightModel;
+import com.apap.tu04.repository.FlightDb;
+
+/**
+ * FlightServiceImpl
+ */
+@Service
+@Transactional
+public class FlightServiceImpl implements FlightService {
+	@Autowired
+	private FlightDb flightDb;
+	
+	@Override
+	public void addFlight(FlightModel flight) {
+		System.out.println(flight.getDestination());
+		System.out.println(flight.getFlightNumber());
+		System.out.println(flight.getOrigin());
+		System.out.println(flight.getPilot().getLicenseNumber());
+		flightDb.save(flight);
+	}
+	
+	@Override
+	public FlightModel getFlightDetailByFlightNumber(String flightNumber) {
+		return flightDb.findByFlightNumber(flightNumber);
+	}
+
+	@Override
+	public List<FlightModel> getFlightList() {
+		// TODO Auto-generated method stub
+		return flightDb.findAll();
+	}
+
+	@Override
+	public void deleteFlight(long id) {
+		flightDb.deleteById(id);
+	}
+
+	@Override
+	public FlightModel findById(long id) {
+		List<FlightModel> flights = flightDb.findAll();
+		FlightModel count = new FlightModel();
+		for(FlightModel flight:flights) {
+			if(flight.getId()==id) {
+				count = flight;
+			}
+		}
+		return count;
+	}
+
+	@Override
+	public FlightModel getFlight(long id) {
+		String idnya = "" + id;
+		return flightDb.findByFlightNumber(idnya);
+	}
+	
+	@Override
+	public void deleteFlightbyId(long flightId) {
+		// TODO Auto-generated method stub
+		flightDb.deleteById(flightId);
+	}
+
+}
